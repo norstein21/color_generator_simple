7b85f811dfb570badc387a200fd92083f6d5e182
norstein21 <habieputra3@gmail.com>
2022-11-14 23:49:11 +0700

    add color generator w/ value library

--- src/App.js
+++ src/App.js
@@ -4,7 +4,52 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('');
+  const [list,setList] = useState([]);
+  const [error,setError] = useState(false);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    try{
+      let colors = new Values(color).all(10);
+      setList(colors)
+      setError(false)
+      console.log(colors)
+    } catch(error){
+      setError(true);
+      console.log(error);
+    }
+  }
+  
+
+  return (
+  <>
+    <section className='container'>
+      <h3>Color Palette App</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        onChange={(e)=>setColor(e.target.value)} 
+        value={color}
+        className={`${error ? 'error' : null}`}
+        >
+        </input>
+        <button 
+        className='btn' type='submit'>
+          Submit
+        </button>
+      </form>
+    </section>
+    <section className='colors'>
+      {list.map ((i,index)=>{
+        console.log(i);
+        return <SingleColor key={index} {...i} index={index} hexColor={i.hex} />
+      })}
+    </section>
+  </>
+  );
+
+  
 }
 
-export default App
+export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({index,rgb,weight,hexColor}) => {
+  const [alert,setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  console.log(bcg)
+
+  return (
+    <article className={`color ${index > 10 && 'color-light'}`} style={{background : `rgb(${bcg})` }}>
+      <p className='color-percent'>{weight}%</p>
+      <p className='color-value'>#{hexColor}</p>
+    </article>
+  );
 }
 
 export default SingleColor
